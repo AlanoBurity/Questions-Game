5d02468456b1d1b7b4cb28d81da41e1a51f790c7
Miyuki Ishii <miyukisabrina@gmail.com>
2022-08-05 18:21:21 -0300

    2 and 3 Requirements

---
+++ src/pages/Questions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Questions extends Component {
+  render() {
+    return (
+      <div>
+        oi
+      </div>
+    );
+  }
+}
+
+export default Questions;
