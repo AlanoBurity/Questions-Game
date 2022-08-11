46c5197a12948f5038f931a5b285fb4488785d0d
Miyuki Ishii <miyukisabrina@gmail.com>
2022-08-11 15:23:25 -0300

    css

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Settings extends Component {
   render() {
     return (
-      <div>
-        <h1 data-testid="settings-title">settings</h1>
+      <div className="settings-section">
+        <h1 data-testid="settings-title">Settings</h1>
+        <p>Em construção...</p>
       </div>
     );
   }
